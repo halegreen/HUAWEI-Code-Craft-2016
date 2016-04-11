fdd5187cc1bda130632bf8cad6abab077351dba6
WXXGREEN <1141195807@qq.com>
2016-04-11 15:28:44 +0800

    second commit

---
+++ SDK-java-0.9/code/future_net/src/com/routesearch/route/Route.java
@@ -0,0 +1,24 @@
+/**
+ * 实现代码文件
+ * 
+ * @author XXX
+ * @since 2016-3-4
+ * @version V1.0
+ */
+package com.routesearch.route;
+
+public final class Route
+{
+    /**
+     * 你需要完成功能的入口
+     * 
+     * @author XXX
+     * @since 2016-3-4
+     * @version V1
+     */
+    public static String searchRoute(String graphContent, String condition)
+    {
+        return "hello world!";
+    }
+
+}
